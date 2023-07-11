c9e06b96df87c025532c187e94cd53b8d66096df
John Brittain <jsb1982@gmail.com>
2023-07-11 15:43:18 +0100

    (wip) Build vocpl pipeline

--- workflows/vocpl/build.sh
+++ workflows/vocpl/build.sh
@@ -9,39 +9,15 @@ mkdir build/resources
 
 pushd build
 
-
-######################################
-### THINGS THAT NEED AUTOMATING... ###
-######################################
-
-# Default target (need dynamic targets; also default target)
-echo "
-
-rule target:
-    input:
-        \"results/vocpl_nextalign_4/s123456/nextalign.aligned.fasta\"
-    default_target: True" >> workflow/Snakefile
-
-# Resources and scripts are inaccessible
-mkdir resources
-# cp ../sources/provide_seeds/resources/seeds.txt resources/seeds.txt
-ln -s ~/Desktop/ex_input resources
-ln -s ex_input/beta.metadata.tsv resources/beta.metadata.tsv
-ln -s ex_input/beta.fasta resources/beta.fasta
-ln -s ../modules/subsample_ids_metadata/scripts scripts
-cp ../../../../../joetsui1994/vocpl/resources/nov_sars-cov-2/* resources
-
-######################################
-
-
-
-
-
-
-
-
 snakemake --d3dag
 snakemake --dag | dot -Tsvg > dag.svg
 open dag.svg
+
+snakemake --list
+snakemake -np
+
+### ISSUE - TODO - Specified default target ###
+snakemake --cores 4 --use-conda vocpl_ml_tree_target
+
 popd
 deactivate
